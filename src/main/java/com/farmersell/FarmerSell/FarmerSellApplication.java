345b46497cfcbb0ba7204e8ebc058cebd8fb5a4d
VivekKumarRapaka <vivekkumar.rapaka@olivecrypto.com>
2025-04-04 17:49:39 +0530

    initial commit

---
+++ src/main/java/com/farmersell/FarmerSell/FarmerSellApplication.java
@@ -0,0 +1,13 @@
+package com.farmersell.FarmerSell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmerSellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmerSellApplication.class, args);
+	}
+
+}
